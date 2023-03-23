95e29c2ab42e3bb214b0aa3052aa3ea3db543beb
Marcell Almeida <almeidamarcell@gmail.com>
2023-03-23 17:12:16 -0300

    upload out

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function (event) {
+  console.log('Hello from Typing Mind!');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request);
+        var cache = await caches.open('cache');
+        cache.put(event.request.url, res.clone());
+        return res;
+      } catch (error) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+});
